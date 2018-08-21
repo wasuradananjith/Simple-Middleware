0900aa6574fb37c99867b887ffd62468474590fb
wasuradananjith <wasuradananjith@gmail.com>
2018-08-21 07:25:03 +0530

    gcd not completed - arguments issue

--- src/middleware/services/ServiceProvider_1.java
+++ src/middleware/services/ServiceProvider_1.java
@@ -14,4 +14,17 @@ public class ServiceProvider_1 {
 	public String greeting(String guest){
 		return "Welcome to ServiceProvider_1 "+guest;
 	}
+	
+	public String gcd(String num) {
+		int n1 = 81, n2 = 153, gcd = 1;
+
+        for(int i = 1; i <= n1 && i <= n2; ++i)
+        {
+            // Checks if i is factor of both integers
+            if(n1 % i==0 && n2 % i==0)
+                gcd = i;
+        }
+        
+        return num;
+	}
 }
